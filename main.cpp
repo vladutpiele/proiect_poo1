f8d88871db4aef092794441804c8e748cd85af0b
vladutpiele <v_piele04@yahoo.com>
2024-04-03 17:15:43 +0300

    commit initial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 
 class Carte
@@ -8,27 +9,25 @@ private:
     std::string nume;
     std::string autor;
     int numarPagini;
+    int nrZileMaxImprumut;
 
 public:
-    Carte(std::string _nume, std::string _autor, int _numarPagini)
+    Carte(std::string _nume, std::string _autor, int _numarPagini, int _numarZileImprumut)
     {
-        nume = _nume;
-        autor = _autor;
+        nume = std::move(_nume);
+        autor = std::move(_autor);
         numarPagini = _numarPagini;
+        nrZileMaxImprumut = _numarZileImprumut;
     }
     Carte(const Carte &other)
     {
         nume = other.nume;
         autor = other.autor;
         numarPagini = other.numarPagini;
+        nrZileMaxImprumut = other.nrZileMaxImprumut;
     }
     Carte &operator=(const Carte &other)
-    {
-        nume = other.nume;
-        autor = other.autor;
-        numarPagini = other.numarPagini;
-        return *this;
-    }
+    = default;
     bool operator==(const Carte &other) const
     {
         return (nume == other.nume) && (autor == other.autor) && (numarPagini == other.numarPagini);
@@ -36,23 +35,29 @@ public:
     friend std::ostream &operator<<(std::ostream &os, const Carte &carte)
     {
         os << "Numele cartii este " << carte.nume << ", autorul ei este " << carte.autor << ", cu numar de pagini : " << carte.numarPagini << '\n';
+        os << "Numarul de zile pana cand cartea trebuie sa fie returnata este" << carte.nrZileMaxImprumut << '\n';
         return os;
     }
-    ~Carte()
-    {
-        /// destructor
-    }
+    ~Carte() = default;
+
     std::string numeCarte()
     {
         return nume;
     }
+    /*
     std::string numeAutor()
     {
         return autor;
     }
-    int numarPaginiCarte()
+
+    [[nodiscard]] int numarPaginiCarte() const
     {
         return numarPagini;
+    }
+     */
+    [[nodiscard]] int maxImprumut() const
+    {
+        return nrZileMaxImprumut;
     }
 };
 
@@ -60,15 +65,15 @@ class Bibliotecar
 {
 private:
     std::string nume;
-    int numarGestiune;
+    int numarGestiune{};
     std::vector<Carte> gestiuneCarti; /// cartile pentru care bibliotecarul este responsabil
 public:
     Bibliotecar()
+    = default;
+
+    [[maybe_unused]] explicit Bibliotecar(std::string _nume)
     {
-    }
-    Bibliotecar(std::string _nume)
-    {
-        nume = _nume;
+        nume = std::move(_nume);
         numarGestiune = 0;
         gestiuneCarti.clear();
     }
@@ -79,13 +84,8 @@ public:
         gestiuneCarti = other.gestiuneCarti;
     }
     Bibliotecar &operator=(const Bibliotecar &other)
-    {
-        nume = other.nume;
-        numarGestiune = other.numarGestiune;
-        gestiuneCarti = other.gestiuneCarti;
-        return *this;
-    }
-    std::string afiseazaNume() const
+    = default;
+    [[nodiscard]] std::string afiseazaNume() const
     {
         return nume;
     }
@@ -99,15 +99,13 @@ public:
         }
         return os;
     }
-    ~Bibliotecar()
-    {
-        /// destructor
-    }
+    ~Bibliotecar() = default;
+
     std::vector<Carte> CartileDinGestiune()
     {
         return gestiuneCarti;
     }
-    void imprumutaCarte(int idx)
+    void imprumutaCarte(unsigned idx)
     {
         numarGestiune--;
         gestiuneCarti.erase(gestiuneCarti.begin() + idx);
@@ -128,9 +126,9 @@ private:
     std::vector<Carte> cartiDeCitit;
 
 public:
-    SalaDeLectura(std::string _nume, int _numarCarti, Bibliotecar _bibliotecar)
+    SalaDeLectura(std::string _nume, int _numarCarti, const Bibliotecar& _bibliotecar)
     {
-        nume = _nume;
+        nume = std::move(_nume);
         numarCarti = _numarCarti;
         bibliotecar = _bibliotecar;
     }
@@ -144,23 +142,15 @@ public:
     }
 
     SalaDeLectura &operator=(const SalaDeLectura &other)
-    {
-        nume = other.nume;
-        bibliotecar = other.bibliotecar;
-        numarCarti = other.numarCarti;
-        cartiDeCitit = other.cartiDeCitit;
-        return *this;
-    }
+    = default;
 
     void adaugaCarte(const Carte &carte)
     {
         cartiDeCitit.push_back(carte);
     }
 
-    ~SalaDeLectura()
-    {
-        /// destructor
-    }
+    ~SalaDeLectura() = default;
+
     friend std::ostream &operator<<(std::ostream &os, const SalaDeLectura &saladelectura)
     {
         os << "Numele salii de lectura este " << saladelectura.nume << ", bibliotecarul responsabil este " << saladelectura.bibliotecar.afiseazaNume() << " care este responsabil de " << saladelectura.numarCarti << " carti" << '\n';
@@ -188,10 +178,10 @@ private:
 public:
     Biblioteca(std::string _nume, int _numarCarti, int _numarBibliotecari, std::vector<SalaDeLectura> _saladelectura)
     {
-        nume = _nume;
+        nume = std::move(_nume);
         numarCarti = _numarCarti;
         numarBibliotecari = _numarBibliotecari;
-        salaDeLectura = _saladelectura;
+        salaDeLectura = std::move(_saladelectura);
     }
     Biblioteca(const Biblioteca &other)
     {
@@ -201,17 +191,9 @@ public:
         salaDeLectura = other.salaDeLectura;
     }
     Biblioteca &operator=(const Biblioteca &other)
-    {
-        nume = other.nume;
-        numarCarti = other.numarCarti;
-        numarBibliotecari = other.numarBibliotecari;
-        salaDeLectura = other.salaDeLectura;
-        return *this;
-    }
-    ~Biblioteca()
-    {
-        /// destructor
-    }
+    = default;
+    ~Biblioteca() = default;
+
     friend std::ostream &operator<<(std::ostream &os, const Biblioteca &biblioteca)
     {
         os << "Numele este " << biblioteca.nume << ", numarul de carti este " << biblioteca.numarCarti << ", iar numarul de bibliotecari este " << biblioteca.numarBibliotecari << '\n';
@@ -229,30 +211,36 @@ class MembruBiblioteca
 private:
     std::string nume;
     std::vector<Carte> cartiImprumutate;
+    int valoareAmenda;
+    int valMaxAmenda;
 
 public:
-    MembruBiblioteca(std::string _nume)
+    explicit MembruBiblioteca(std::string _nume, int _valoareAmenda, int _valMaxAmenda)
     {
-        nume = _nume;
+        nume = std::move(_nume);
+        valoareAmenda = _valoareAmenda;
+        valMaxAmenda = _valMaxAmenda;
     }
     MembruBiblioteca(const MembruBiblioteca &other)
     {
         nume = other.nume;
         cartiImprumutate = other.cartiImprumutate;
+        valoareAmenda = other.valoareAmenda;
+        valMaxAmenda = other.valMaxAmenda;
     }
     MembruBiblioteca &operator=(const MembruBiblioteca &other)
-    {
-        nume = other.nume;
-        cartiImprumutate = other.cartiImprumutate;
-        return *this;
-    }
+    = default;
+    ~MembruBiblioteca() = default;
+
     friend std::ostream &operator<<(std::ostream &os, const MembruBiblioteca &membrubiblioteca)
     {
+        os << membrubiblioteca.nume << "are o amenda de " << membrubiblioteca.valoareAmenda << " lei" << '\n';
         os << membrubiblioteca.nume << " a imprumutat urmatoarele carti " << '\n';
         for (Carte it : membrubiblioteca.cartiImprumutate)
         {
             os << it.numeCarte() << '\n';
         }
+
         return os;
     }
     void imprumutaCarte(Carte &carte, Bibliotecar &bibliotecar)
@@ -265,17 +253,19 @@ public:
                 return;
             }
         }
-        int index = -1;
+        unsigned index = bibliotecar.CartileDinGestiune().size();
         std::vector<Carte> w = bibliotecar.CartileDinGestiune();
-        for (int i = 0; i < w.size(); i++)
+        bool ok = false;
+        for (unsigned i = 0; i < w.size(); i++)
         {
             Carte it = w[i];
             if (it == carte)
             {
+                ok = true;
                 index = i;
             }
         }
-        if (index == -1)
+        if (!ok)
         {
             std::cout << carte.numeCarte() << " nu poate fi imprumutata pentru ca nu exista." << '\n';
             return;
@@ -283,42 +273,49 @@ public:
         cartiImprumutate.push_back(carte);
         bibliotecar.imprumutaCarte(index);
     }
-    void returneazaCarte(Carte &carte, Bibliotecar &bibliotecar)
-    {
-        int index = -1;
-        for (int i = 0; i < cartiImprumutate.size(); i++)
-        {
+    void returneazaCarte(Carte &carte, Bibliotecar &bibliotecar, int nrZileImprumut) {
+        unsigned index = -1;
+        for (unsigned i = 0; i < cartiImprumutate.size(); i++) {
             Carte it = cartiImprumutate[i];
-            if (it == carte)
-            {
+            if (it == carte) {
                 index = i;
                 break;
             }
         }
-        if (index > -1)
-        {
+        if (index > -1) {
+            if (nrZileImprumut > carte.maxImprumut()) {
+                valoareAmenda += 100 * (nrZileImprumut - carte.maxImprumut());
+                std::cout << nume << " a fost amendat pentru returnarea cu intarziere a cartii" << '\n';
+            }
             cartiImprumutate.erase(cartiImprumutate.begin() + index);
             bibliotecar.adaugaCarte(carte);
+        } else {
+            std::cout << "Cartea " << carte.numeCarte() << " nu poate fi returnata daca nu a fost imprumutata de "
+                      << nume << '\n';
         }
-        else
+    }
+    void verificaAmenda()
+    {
+        if(valoareAmenda > valMaxAmenda)
         {
-            std::cout << "Cartea " << carte.numeCarte() << " nu poate fi returnata daca nu a fost imprumutata de " << nume << '\n';
+            std::cout << nume << " a fost amendat de prea multe ori deci nu va mai putea imprumuta carti din biblioteca" <<
+            '\n';
         }
     }
 };
 
 int main()
 {
-    Carte carte1("Luceafarul", "Mihai Eminescu", 100);
-    Carte carte2("Amintiri din Copilarie", "Ion Creanga", 200);
-    Carte carte3("Morometii", "Marin Preda", 400);
-    Carte carte4("Patul lui Procust", "Camil Petrescu", 178);
-    Carte carte5("Pacala", "Ion Creaga", 87);
-    Carte carte6("Ursul Pacalit de Vulpe", "Ion Creanga", 55);
-    Carte carte7("Baltagul", "Mihail Sadoveanu", 133);
-    Carte carte8("Enigma Otiliei", "George Calinescu", 315);
-    Carte carte9("Ion", "Liviu Rebreanu", 478);
-    Carte carte10("Moara cu Noroc", "Ioan Slavici", 555);
+    Carte carte1("Luceafarul", "Mihai Eminescu", 100, 20);
+    Carte carte2("Amintiri din Copilarie", "Ion Creanga", 200, 15);
+    Carte carte3("Morometii", "Marin Preda", 400, 14);
+    Carte carte4("Patul lui Procust", "Camil Petrescu", 178, 6);
+    Carte carte5("Pacala", "Ion Creaga", 87, 21);
+    Carte carte6("Ursul Pacalit de Vulpe", "Ion Creanga", 55, 7);
+    Carte carte7("Baltagul", "Mihail Sadoveanu", 133, 21);
+    Carte carte8("Enigma Otiliei", "George Calinescu", 315, 14);
+    Carte carte9("Ion", "Liviu Rebreanu", 478, 14);
+    Carte carte10("Moara cu Noroc", "Ioan Slavici", 555, 14);
 
     std::cout << carte3 << '\n';
     std::cout << carte8 << '\n';
@@ -340,15 +337,16 @@ int main()
     bibliotecar2.adaugaCarte(carte10);
     std::cout << bibliotecar2 << '\n';
 
-    MembruBiblioteca membrubiblioteca1("Vladut Piele");
+    MembruBiblioteca membrubiblioteca1("Vladut Piele", 0, 1000);
     membrubiblioteca1.imprumutaCarte(carte1, bibliotecar1);
     membrubiblioteca1.imprumutaCarte(carte3, bibliotecar1);
     std::cout << membrubiblioteca1 << '\n';
-    membrubiblioteca1.returneazaCarte(carte2, bibliotecar1);
+    membrubiblioteca1.returneazaCarte(carte2, bibliotecar1, 15);
     /// cartea nu a fost imprumutata deci afiseaza "cartea nu poate fi returnata daca .... nu a fost imprumutata".
     std::cout << '\n';
     membrubiblioteca1.imprumutaCarte(carte1, bibliotecar1);
     ///  a imprumutat deja cartea1, deci afiseaza "cartea a fost deja imprumutata..."
+    membrubiblioteca1.verificaAmenda();
 
     SalaDeLectura saladelectura1("Sala de lectura copii", 5, bibliotecar1);
     SalaDeLectura saladelectura2("Sala de lectura adulti", 5, bibliotecar2);
